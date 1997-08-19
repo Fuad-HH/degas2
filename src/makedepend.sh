619b25ddfe0ed942bbbb847298c68ec61bb4cd08
Charles Karney <charles.karney@sri.com>
1997-08-19 14:37:12 +0000

    Changes to support compilation under f90.  This involved extensive changes in array.hweb and sysdep.web.  Nearly all other files needed some modifications.

--- src/makedepend.sh
+++ src/makedepend.sh
@@ -23,8 +23,10 @@ temp=/tmp/makedepend_$$ # basename for temporary files
 # $temp.c a temporary file
 # $temp.d holds accumulated includes for all files
 
+# $temp.e holds F90 stuff
 
 cp /dev/null $temp.d
+
 for file do
     echo $file > $temp.a # Start things off by assuming "@i $file" is given.
     cp /dev/null $temp.b # No accumulated includes yet
@@ -45,13 +47,49 @@ for file do
     grep -v "^$file\$" $temp.b > $temp.c # Remove head file.
     mv $temp.c $temp.b
     # Create a Makefile dependency line
-    test -s $temp.b && echo `echo $file | sed 's/\.[^.]*$/.f/'`: `cat $temp.b` >>$temp.d
+    case $file in
+        *.web ) test -s $temp.b && echo `echo $file | sed 's/\.[^.]*$/.f/'`: `cat $temp.b` >>$temp.d ;;
+	*.hweb ) test -s $temp.b && grep '^[ 	]*package_init' $file > /dev/null && echo `echo $file | sed 's/\.[^.]*$/_mod.f/'`: `cat $temp.b` >>$temp.d ;;
+    esac
 done
-mv Makefile Makefile.bak
 
-sed -e '/^# DO NOT DELETE/,$d' Makefile.bak > Makefile
-echo "# DO NOT DELETE THIS LINE -- make depend depends on it." >> Makefile
-echo 'ifeq ($(STANDALONE),yes)' >> Makefile
-cat $temp.d >> Makefile
-echo 'endif' >> Makefile
+# Do FORTRAN 90 depends
+cp /dev/null $temp.e
+for file do
+    case $file in 
+     *.hweb )
+	mods=`grep "^[ 	]*package_init\([a-z0-9]*\)" $file | grep -v '@m' | tr -s '; 	/*!' '\012' | grep package_init | sort -u | sed -e 's/)/_suffix/' -e 's/^package_init(/prefix_/'`
+	if test "$mods"; then
+#	echo `echo $mods | sed -e 's/prefix_//' -e 's/_suffix/.mod/'`: `echo $file | sed 's/\.[^.]*$/_mod.f/'` >> $temp.e
+	for mod in $mods;do
+	    echo `echo $mod | sed -e 's/prefix_//' -e 's/_suffix/_MOD/'`:=`echo $file | sed 's/\.[^.]*$/_mod.$O/'` >> $temp.e
+	 done
+	fi
+       ;;
+    esac
+done
+
+for file do
+    case $file in 
+    *.web )
+       mods=`grep "^[ 	]*[a-z0-9]*_common" $file | tr -s '; 	/*!' '\012' | grep _common | sort -u | sed -e 's/_common/_suffix/' -e 's/^/prefix_/'`
+       if test "$mods"; then
+	echo `echo $file | sed 's/\.[^.]*$/_mods/'`:= `echo $mods | sed -e 's/prefix_/$(/g' -e 's/_suffix/_MOD)/g'` >> $temp.e
+#	echo `echo $file | sed 's/\.[^.]*$/.o/'`: `echo $mods | sed -e 's/prefix_/$(/g' -e 's/_suffix/_MOD)/'` >> $temp.e
+	echo `echo $file | sed 's/\.[^.]*$/.o/'`: '$('`echo $file | sed 's/\.[^.]*$/_mods/'`')' >> $temp.e
+       fi
+       ;;
+    esac
+done
+
+
+(
+echo 'ifeq ($(FORTRAN90),yes)'
+cat $temp.e
+echo endif
+echo 'ifeq ($(STANDALONE),yes)'
+cat $temp.d
+echo 'endif'
+) > Makefile.depends
+
 rm -rf $temp.*
