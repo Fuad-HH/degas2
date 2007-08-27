bc7f569a9eaa4fff40b208fae04f1f914a04f675
Daren Stotler <dstotler@pppl.gov>
2007-08-27 15:04:43 +0000

    Add code for chord extension and third dimension.

--- dg_carre/DG/src/chords.c
+++ dg_carre/DG/src/chords.c
@@ -4,7 +4,11 @@ typedef struct _ChangeChordRec {
   ActProc actProc;
   void* obj;
   Chord ch;
+#ifdef CHORDZ /* add z to struct _ChangeChordRec */
+  double x1,y1,x2,y2,z1,z2;
+#else
   double x1,y1,x2,y2;
+#endif
 }* ChangeChordRec;
 
 static int ActAddChord(App a,ActRec ar);
@@ -78,6 +82,87 @@ void TlAddChord(View w,int event,double x,double y) {
   w->toolData=d;
 }
 
+#ifdef CHORDEXT /* define TlExtChord */
+void TlExtChord(View w,int event,double x,double y) {
+  struct {
+    int bEnh,hm;
+    Group g;
+    Chord ch;
+  }* d;
+  void* p = NULL;
+  Group g;
+  MeshPoint mpt;
+  MeshCell mc;
+  Index ix;
+  int i;
+
+  d=w->toolData;
+  switch(event) {
+    case TL_DISABLE:
+      if (d!=NULL) FatalError("Tool()-disable0: fatal error 2");
+    case TL_ENABLE:
+      break;
+    case TL_DBLCLK:
+      if (d!=NULL) break;
+      break;
+    case TL_PRESS:
+      if (d!=NULL) break;
+      d=Malloc(sizeof(*d));
+      d->bEnh=0;
+      d->hm=1;
+      d->ch=NULL;
+      SetViewFlags(w,w->showFlags | SHW_CHORDS);
+      p=HitViewObject(w,x,y,SHW_CHORDS);
+      if (p==NULL) {
+        d=Free(d);
+        break;
+      }
+
+    case TL_MOTION:
+      if (d==NULL) break;
+      if (d->bEnh) {
+      } else {
+        if (d->ch!=NULL) p=HitViewObject(w,x,y,SHW_CHORDS);
+
+        if (p==NULL || p==d->ch) break;
+        d->ch=p;
+
+        UnhighlightAll(w);
+        SetHighlightMode(w->app,1);
+      }
+      break;
+
+    case TL_RELEASE:
+      if (d==NULL) break;
+      if (d->bEnh) {
+      } else {
+        if (d->ch!=NULL) /* eventually replace this with an error thing */
+	  if (ExtendChord(w->app,d->ch))
+	    SetViewMsg(w,GetStr(w,ERR_NOELEMSINPATH));
+      }
+      UnhighlightAll(w);
+      UndoMark(w->app);
+      d=Free(d);
+      break;
+    case TL_ENTER:
+    case TL_LEAVE:
+      if (d==NULL) break;
+      if (d->hm) SetHighlightMode(w->app,event==TL_ENTER);
+      break;
+    case TL_CANCEL:
+      if (d==NULL) break;
+      if (d->bEnh) {
+      }
+      d=Free(d);
+      ViewMsgEx(w,MSG_CANCELED,NULL);
+      Cancel(w->app);
+      break;
+  }
+  FlushView(w);
+  w->toolData=d;
+}
+#endif
+
 
 Chord AddChord(App a,double x1,double y1,double x2,double y2) {
   Chord ch;
@@ -93,6 +178,10 @@ Chord AddChord(App a,double x1,double y1,double x2,double y2) {
   ch->y1=y1;
   ch->x2=x2;
   ch->y2=y2;
+#ifdef CHORDZ /* initialize dummy z in AddChord */
+  ch->z1=0;
+  ch->z2=0;
+#endif
   ch->vars=ch->varsContaining=NULL;
 
   ar.obj=ch;
@@ -109,6 +198,10 @@ int ChangeChord(App a,Chord ch,double x1,double y1,double x2,double y2) {
   ar.y1=y1;
   ar.x2=x2;
   ar.y2=y2;
+#ifdef CHORDZ /* initialize dummy z in ChangeChord */
+  ar.z1=ch->z1;
+  ar.z2=ch->z2;
+#endif
 
   ActChangeChord(a,&ar);
 
@@ -135,6 +228,65 @@ void* DelChord(App a,Chord ch) {
 
   return NULL;
 }
+
+#ifdef CHORDZ /* define AddChord3D, ChangeChord3D, and DrawChord3D */
+Chord AddChord3D(App a,double x1,double y1,double x2,double y2,double z1,
+    double z2) {
+  Chord ch;
+  struct _ActRec ar;
+
+  if (x1==x2 && y1==y2) return NULL;
+
+  ch=Malloc(sizeof(*ch));
+
+  ch->type=T_CHORD;
+  ch->locks=0;
+  ch->x1=x1;
+  ch->y1=y1;
+  ch->x2=x2;
+  ch->y2=y2;
+  ch->z1=z1;
+  ch->z2=z2;
+  ch->vars=ch->varsContaining=NULL;
+
+  ar.obj=ch;
+  ActAddChord(a,&ar);
+
+  return ch;
+}
+
+int ChangeChord3D(App a,Chord ch,double z1,double z2) {
+  struct _ChangeChordRec ar;
+
+  ar.ch=ch;
+  ar.x1=ch->x1;
+  ar.x2=ch->x2;
+  ar.y1=ch->y1;
+  ar.y2=ch->y2;
+  ar.z1=z1;
+  ar.z2=z2;
+
+  ActChangeChord(a,&ar);
+
+  return 0;
+}
+
+#define CHORD_RES 20
+
+static void DrawChord3D(View w,Chord ch) {
+  double i,x1,y1,x2,y2;
+
+  x1=hypot(ch->x1,ch->z1);y1=ch->y1;
+  for (i=1./CHORD_RES;i<1;i+=1./CHORD_RES) {
+    x2=hypot(ch->x1+i*(ch->x2-ch->x1),ch->z1+i*(ch->z2-ch->z1));
+    y2=ch->y1+i*(ch->y2-ch->y1);
+    DrawViewLine(w,x1,y1,x2,y2);
+    x1=x2;y1=y2;
+  }
+  x2=hypot(ch->x2,ch->z2);y2=ch->y2;
+  DrawViewLine(w,x1,y1,x2,y2);
+}
+#endif
 
 void DrawChord(View w,Chord ch,int mode) {
   double x,y,l;
@@ -171,6 +323,9 @@ void DrawChord(View w,Chord ch,int mode) {
         SetViewMode(w,VM0_ELEMMARK);
         break;
     }
+#ifdef CHORDZ /* insert the 3D drawing procedure */
+    if (ch->z1!=0 || ch->z2!=0)  DrawChord3D(w,ch); else
+#endif
     DrawViewLine(w,ch->x1,ch->y1,ch->x2,ch->y2);
   }
   switch(mode) {
@@ -183,8 +338,46 @@ void DrawChord(View w,Chord ch,int mode) {
       SetViewMode(w,VM0_CHORD);
       break;
   }
+#ifdef CHORDZ /* insert the 3D drawing procedure */
+  if (ch->z1!=0 || ch->z2!=0)  DrawChord3D(w,ch); else
+#endif
   DrawViewLine(w,ch->x1,ch->y1,ch->x2,ch->y2);
 }
+
+#ifdef CHORDEXT /* define ExtendChord procedure */
+int ExtendChord(App a,Chord ch) {
+  double x,y;
+  double d,d0,d1,ra,rb;
+  Group g;
+  void* obj;
+  Elem e;
+  Index ix,ixe;
+
+  g=CreateGroup();
+
+  for (obj=Group1st(a->mark,&ix);obj!=NULL;obj=Next(&ix))
+    if (GetObjType(obj)==T_ELEM) GroupAdd(g,obj);
+  if (IsEmptyGroup(g))  g=CopyGroup(a->elems,g);
+
+  d0=Point2PointDist(ch->x1,ch->y1,ch->x2,ch->y2);
+
+  d1=MAXDOUBLE;
+  for (e=Group1st(g,&ixe);e!=NULL;e=Next(&ixe)) {
+    VIntersect(ch->x1,ch->y1,ch->x2,ch->y2,
+      e->n[1]->x,e->n[1]->y,e->n[2]->x,e->n[2]->y,&ra,&rb);
+    if (ra<0 || rb<0 || rb>1) continue; /* the element does not intersect */
+    d=Point2PointDist(ch->x1,ch->y1,ch->x1+(ch->x2-ch->x1)*ra,
+      ch->y1+(ch->y2-ch->y1)*ra);
+    if (d<d1) d1=d;
+  }
+  FreeGroup(g);
+
+  if (d1==MAXDOUBLE) return ERR_NOELEMSINPATH;
+  ChangeChord(a,ch,ch->x1,ch->y1,ch->x1+d1/d0*(ch->x2-ch->x1),
+    ch->y1+d1/d0*(ch->y2-ch->y1));
+  return 0;
+}
+#endif
 
 static int ActAddChord(App a,ActRec ar) {
   DelRec ur;
@@ -260,6 +453,10 @@ static int ActChangeChord(App a,ChangeChordRec ar) {
   ur->y1=ar->ch->y1;
   ur->x2=ar->ch->x2;
   ur->y2=ar->ch->y2;
+#ifdef CHORDZ /* add dummy z to UndoRec */
+  ur->z1=ar->ch->z1;
+  ur->z2=ar->ch->z2;
+#endif
 
   NotifyChange(a,ar->ch);
   DrawAppHighlight(a,DRAW_OFF);
@@ -270,6 +467,10 @@ static int ActChangeChord(App a,ChangeChordRec ar) {
   ar->ch->y1=ar->y1;
   ar->ch->x2=ar->x2;
   ar->ch->y2=ar->y2;
+#ifdef CHORDZ /* pass dummy z to new chord coordinates */
+  ar->ch->z1=ar->z1;
+  ar->ch->z2=ar->z2;
+#endif
 
   DrawAppObject(a,ar->ch,DRAW_ON);
 
@@ -341,5 +542,4 @@ int ConvertTemplateToChords(App a) {
   }
   return 0;
 }
-
 
--- dg_carre/DG/src/chords.h
+++ dg_carre/DG/src/chords.h
@@ -7,15 +7,26 @@ struct _Chord {
   int type,locks;
   double x1,y1,x2,y2;
   Group vars,varsContaining;
+#ifdef CHORDZ /* add z to struct _Chord */
+  double z1,z2;
+#endif
 };
 
 Chord AddChord(App a,double x1,double y1,double x2,double y2);
 int ChangeChord(App a,Chord ch,double x1,double y1,double x2,double y2);
 void* DelChord(App a,Chord ch);
+#ifdef CHORDZ /* declare AddChord3D and ChangeChord3D */
+Chord AddChord3D(App a,double x1,double y1,double x2,double y2,double z1,
+  double z2);
+int ChangeChord3D(App a,Chord ch,double z1,double z2);
+#endif
 
 void DrawChord(View w,Chord ch,int mode);
 
 void TlAddChord(View w,int event,double x,double y);
+#ifdef CHORDEXT /* declare TlExtChord */
+void TlExtChord(View w,int event,double x,double y);
+#endif
 
 int ConvertElemsToChords(App a,Group elems,void** pErrObj);
 int ConvertTemplateToChords(App a);
@@ -23,5 +34,8 @@ int ConvertTemplateToChords(App a);
 /* Direction does not matter! */
 Chord FindChord(App a,double x1,double y1,double x2,double y2);
 
+#ifdef CHORDEXT /* declare ExtendChord */
+int ExtendChord(App a,Chord ch); /* extend the chord to end on an element */
+#endif
 
 #endif
