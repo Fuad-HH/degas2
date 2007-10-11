610a002db9914ba0cf50c941aa13efd61fc57e3d
Daren Stotler <dstotler@pppl.gov>
2007-10-11 14:58:03 +0000

    Add capabilities for adding, drawing, extending and moving 3-D chords.

--- dg_carre/DG/src/chords.h
+++ dg_carre/DG/src/chords.h
@@ -5,37 +5,29 @@
 
 struct _Chord {
   int type,locks;
-  double x1,y1,x2,y2;
+  double x1,y1,z1,x2,y2,z2;
   Group vars,varsContaining;
-#ifdef CHORDZ /* add z to struct _Chord */
-  double z1,z2;
-#endif
 };
 
 Chord AddChord(App a,double x1,double y1,double x2,double y2);
 int ChangeChord(App a,Chord ch,double x1,double y1,double x2,double y2);
 void* DelChord(App a,Chord ch);
-#ifdef CHORDZ /* declare AddChord3D and ChangeChord3D */
-Chord AddChord3D(App a,double x1,double y1,double x2,double y2,double z1,
+Chord AddChord3D(App a,double x1,double y1,double z1,double x2,double y2,
   double z2);
-int ChangeChord3D(App a,Chord ch,double z1,double z2);
-#endif
+int ChangeChord3D(App a,Chord ch,double x1,double y1,double z1,double x2,
+  double y2,double z2);
 
 void DrawChord(View w,Chord ch,int mode);
 
 void TlAddChord(View w,int event,double x,double y);
-#ifdef CHORDEXT /* declare TlExtChord */
 void TlExtChord(View w,int event,double x,double y);
-#endif
+void TlAdjustChord(View w,int event,double x,double y);
 
 int ConvertElemsToChords(App a,Group elems,void** pErrObj);
 int ConvertTemplateToChords(App a);
 
+int ExtendChord(App a,Chord ch); /* extend the chord to end on an element */
+
 /* Direction does not matter! */
 Chord FindChord(App a,double x1,double y1,double x2,double y2);
-
-#ifdef CHORDEXT /* declare ExtendChord */
-int ExtendChord(App a,Chord ch); /* extend the chord to end on an element */
-#endif
-
 #endif
