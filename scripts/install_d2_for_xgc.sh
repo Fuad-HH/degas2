9b510b96f8df7a54165b0dcb1301c33ace4079dd
George J. Wilkie <george.wilkie@gmail.com>
2020-07-10 15:24:33 -0400

    Forced Makefile.local to not corss-compile

--- scripts/install_d2_for_xgc.sh
+++ scripts/install_d2_for_xgc.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# 
+# Usage:
+# ./install_d2_for_xgc.sh SYSTEM
+#
+# Installs DEGAS2 for use with XGC using typical parameters
+# Run the script with an argument for the SYSTEM being used (as recognized by DEGAS2). Nowadays, this is usually LINUX64, but can also be MACOS, SUN, etc. Defaults to LINUX64 if no argument is given.
+#
+# Be sure to define the DEGASROOT environment variable that points to the degas2 directory.
 
 if [ $# -eq 0 ]
 then
@@ -20,6 +28,7 @@ make defineback
 make randomtest
 ./randomtest
 ./datasetup
+cd ../scripts
 
 echo "*** Installation complete. ***"
 echo "Add the line export DEGASROOT=$DEGASROOT to your shell init script (e.g., ~/.bashrc)"
