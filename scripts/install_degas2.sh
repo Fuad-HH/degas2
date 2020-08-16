ac75f52ebbbcfcdd3c9c12e0af6a1de72adf26f1
George J. Wilkie <george.wilkie@gmail.com>
2020-08-16 15:12:46 -0400

    Debugging install script

--- scripts/install_degas2.sh
+++ scripts/install_degas2.sh
@@ -2,67 +2,61 @@
 # Sets up the environment for running degas2
 #
 # Usage, from this directory:
-# ./setup_degas2_environment SYSTEM COMPILER
+# SYSTEM=LINUX64 COMPILER=GFORTRAN MPI_OPT=yes NETCDF_HOME=/usr/local/lib ./install_degas2.sh
 # where SYSTEM is the degas2-specified system (LINUX64 by default), 
-# and COMPILER is the compiler to use (gfortran by default)
+# COMPILER is the compiler to use (gfortran by default), and
+# NETCDF_HOME is where libnetcdf.a and libnetcdff.a are located
 
-if [ $# -eq 0 ]
-then
-   SYSTEM=LINUX64
-   COMPILER=gfortran
-elif [ $# -eq 1 ]
-   SYSTEM=$1
-   COMPILER=gfortran
-else
-   SYSTEM=$1
-   COMPILER=$2
-fi
-
-if [-z "$SYSTEM"]
+if [ -z $SYSTEM ]
 then
    SYSTEM=LINUX64
 fi
 
-if [-z "$COMPILER"]
+
+if [ -z $COMPILER ]
 then
-   COMPILER=gfortran
+   COMPILER=GFORTRAN
 fi
 
-if [-z "$MPI_OPT"]
+if [ -z "$MPI_OPT" ]
 then
    MPI_OPT=no
 fi
 
 cd ..
-export DEGASROOT=$PWD/$SYSTEM
+export DEGASROOT=$PWD
 echo $SYSTEM > this_system
 mkdir $SYSTEM
 cd $SYSTEM
-cp src/Makefile $SYSTEM/
+ln -s ../src/Makefile .
+cd $DEGASROOT
 
-if [ $?NETCDF_C_HOME ]
+if [ ! -z $NETCDF_C_HOME ]
 then
-   NCLIB=-L$NETCDF_C_HOME -lnetcdf
+   NCLIB="-L$NETCDF_C_HOME -lnetcdf"
    if [ $?NETCDF_FORTRAN_HOME ]
-      NCLIB=-L$NETCDF_FORTRAN_HOME -lnetcdff
+   then
+      NCLIB="-L$NETCDF_FORTRAN_HOME -lnetcdff"
    else
       echo "Need to specify NETCDF_FORTRAN_HOME along with NETCDF_C_HOME"
    fi
-elif [ $?NETCDF_HOME ]
+elif [ ! -z $NETCDF_HOME ]
+then
    NCLIB=-L$NETCDF_HOME -lnetcdf -lnetcdff 
-else [ $SYSTEM -eq 'MACOS' ]
-   if test -f "/usr/local/lib/libnetcdff.a"
-      NCLIB=-L/usr/local/lib -lnetcdf -lnetcdff
+else 
+   if [ -f "/usr/local/lib/libnetcdff.a" ]
+   then
+      NCLIB="-L/usr/local/lib -lnetcdf -lnetcdff"
    else
       echo "Need to specify NETCDF_HOME"
    fi
 fi
 
-sed "s/COMPILER_OPT/$COMPILER/g" scripts/templates/Makefile.local | sed "s/MPI_OPT/$MPI_OPT/g" | sed "s/NCLIB/$NCLIB/g" > $SYSTEM/Makefile.local
+sed "s/COMPILER_OPT/$COMPILER/g" $DEGASROOT/scripts/templates/Makefile.local | sed "s/MPI_OPT/$MPI_OPT/g" | sed "s#NCLIB#$NCLIB#g" > $DEGASROOT/$SYSTEM/Makefile.local
 
-mkdir refdata
+mkdir $DEGASROOT/refdata
 
-cd $SYSTEM
+cd $DEGASROOT/$SYSTEM
 make datasetup
 make problemsetup
 make boxgen
@@ -74,23 +68,20 @@ make tri_to_sonnet
 cd ..
 
 # Replace DATADIR with the appropriate directory in template input files
-cp templates/elements.input refdata/
-cp templates/species.input refdata/
-cp templates/materials.input refdata/
-sed -i "s#DATADIR#$PWD/refdata#g" templates/reactions.input > refdata/reactions.input
-sed -i "s#DATADIR#$PWD/refdata#g" templates/pmi.input > refdata/pmi.input
-
-sed -i "s#DATADIR#$PWD/refdata#g" templates/degas2.in > $SYSTEM/degas2.in
-
+cp $DEGASROOT/scripts/templates/elements.input refdata/
+cp $DEGASROOT/scripts/templates/species.input refdata/
+cp $DEGASROOT/scripts/templates/materials.input refdata/
+sed "s#DATADIR#$DEGASROOT/refdata#g" $DEGASROOT/scripts/templates/reactions.input > $DEGASROOT/refdata/reactions.input
+sed "s#DATADIR#$DEGASROOT/refdata#g" $DEGASROOT/scripts/templates/pmi.input > $DEGASROOT/refdata/pmi.input
+sed "s#DATADIR#$DEGASROOT/refdata#g" $DEGASROOT/scripts/templates/degas2.in > $DEGASROOT/$SYSTEM/degas2.in
 
 # Create reference data
-cd $SYSTEM
-./datasetup
+$DEGASROOT/$SYSTEM/datasetup
 
-cd ../scripts
+cd $DEGASROOT/scripts
 
 echo "*** Installation complete. ***"
 echo "Add the line export DEGASROOT=$DEGASROOT to your shell init script (e.g., ~/.bashrc)."
-echo "Template input files (degas2.in, d2problem.input, d2tally.input), which points to semipermanent reference data created by datasetup, is in the directory $DEGASROOT/LINUX64"
+echo "Template input files (degas2.in, d2problem.input, d2tally.input), which point to semipermanent reference data created by datasetup, are in the directory $DEGASROOT/LINUX64"
 
 
