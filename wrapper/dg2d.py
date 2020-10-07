8cfa71896410f1456f645d079bebba57d3362591
George J. Wilkie <george.wilkie@gmail.com>
2020-10-07 15:38:05 -0400

    Added logic to build polygons from wallfiles for LTX topology. Work in progress.

--- wrapper/dg2d.py
+++ wrapper/dg2d.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import polygon
+import sys
 
-def write_dg2d_input_from_wallfile(wallfile_name,dg2dfile_name="dg2d.in",direction="clockwise",debug=False):
+def write_dg2d_input_from_wallfile(wallfile_name,topology,topology_params,dg2dfile_name="dg2d.in",debug=False):
 
     # Read the wallfile
     wallfile = open(wallfile_name,'r')
@@ -67,5 +68,147 @@ def write_dg2d_input_from_wallfile(wallfile_name,dg2dfile_name="dg2d.in",directi
     dg2dfile.write("end_prep\n")
 
     # Now, the polygons need to be generated. Some decisions need to communicated about the topology.
-    polygons = polygon.get_polygons() 
     
+    if topology == "LTX":
+        # For LTX, the walls should be defined in the following order:
+        # 1: External solid wall
+        # 2: Several closed flux surfaces, starting from the outermost
+        num_closed_surfaces = topology_params[0]
+        # 3: The remainder are open flux surfaces (outside the limiter), starting with the outermost
+        num_open_surfaces = nwalls-1-num_closed_surfaces
+
+        clockwise = False
+
+        # The vertex indices of the solid wall which correspond to the limiter edges.
+        # MUST go from top to bottom (high to low index, when listed counterclockwise)
+        limiter_vertex_idx = (topology_params[1], topology_params[2])
+
+        limlcfs_idx_upper = walls[1].insert_vertex_into_surface(walls[0].vertices[limiter_vertex_idx[0]])
+        limlcfs_idx_lower = walls[1].insert_vertex_into_surface(walls[0].vertices[limiter_vertex_idx[1]])
+
+        polys=[]
+
+        ############
+        # First, define the closed flux surface zones
+        for ipoly in range(0,num_closed_surfaces)
+            polys.append( Polygon() )
+            for vertex in walls[1+ipoly].vertices:
+                polys[ipoly].add_vertex(vertex)
+            polys[ipoly].add_vertex(walls[1+ipoly].vertices[0])
+            if ipoly < num_closed_surfaces-1:
+                for vertex in walls[1+ipoly+1].vertices[-1::-1]:
+                    polys[ipoly].add_vertex(vertex)
+                polys[ipoly].add_vertex(walls[1+ipoly+1].vertices[0])
+                polys[ipoly].add_vertex(walls[1+ipoly].vertices[0])
+
+        ############
+        # Then, the limiter region beyond the LCFS
+        polys.append(Polygon())
+
+        # The relevant part of the solid wall
+        for vertex in wall[0].vertices[limiter_vertex_idx[0]:-1]:
+            polys[num_closed_surfaces].add_vertex(vertex)
+        for vertex in wall[0].vertices[0:limiter_vertex_idx[1]]:
+            polys[num_closed_surfaces].add_vertex(vertex)
+        # The relevant part of the LCFS, in reverse order
+        for vertex in wall[1].vertices[limlcfs_idx_lower::-1]:
+            polys[num_closed_surfaces].add_vertex(vertex)
+        for vertex in wall[1].vertices[-1:limlcfs_idx_upper:-1]:
+            polys[num_closed_surfaces].add_vertex(vertex)
+        polys[num_closed_surfaces].add_vertex(wall[0].vertices[limiter_vertex_idx[0])
+
+
+        ############
+        # Then, the zone between the LCFS and the first open flux surface (last in the list of walls)
+        polys.append(Polygon())
+
+        # The relevant part of the LCFS
+        for vertex in wall[1].vertices[limlcfs_idx_lower:limlcfs_idx_upper+1]:
+            polys[num_closed_surfaces+1].add_vertex(vertex)
+        # Now, find the solid wall index corresponding to the ends of the first open flux surfaces
+
+        # First build a dummy polygon to track other surface
+        dummyPoly = Polygon(increment=False)
+        for vertex in wall[-1].vertices(-1::-1):
+            dummyPoly.add_vertex(vertex)
+        wallidx_intersect_lower = dummyPoly.get_next_vertex_extrapolated_to_wall(wall[0])
+        dummyPoly = Polygon(increment=False)
+        for vertex in wall[-1].vertices:
+            dummyPoly.add_vertex(vertex)
+        wallidx_intersect_upper = dummyPoly.get_next_vertex_extrapolated_to_wall(wall[0])
+
+        # Add relevant part of upper solid wall
+        for vertex in wall[0].indices[limiter_vertex[0]:wallidx_intersect_upper-1:-1]:
+            polys[num_closed_surfaces+1].add_vertex(vertex)
+
+        # Add first open flux surface, in reverse order
+        for vertex in wall[-1].indices[-1::-1]:
+            polys[num_closed_surfaces+1].add_vertex(vertex)
+
+        # Add relevant part of lower solid wall
+        for vertex in wall[0].indices[limiter_vertex[1]:wallidx_intersect_lower-1:-1]:
+            polys[num_closed_surfaces+1].add_vertex(vertex)
+
+        # Close the polygon
+        polys[num_closed_surfaces+1].add_vertex(wall[1].vertices[limlcfs_idx_lower])
+
+        ############
+        # Then, the zone between the outermost flux surface and the solid wall. Similar to above
+        polys.append(Polygon())
+
+        # Add the outermost flux surface
+        for vertex in wall[num_closed_surfaces].vertices():
+            polys[num_closed_surfaces+2].add_vertex(vertex)
+
+        # Now, find the solid wall index corresponding to the ends of the first open flux surfaces
+        wallidx_intersect_lower = polys[num_closed_surfaces+2].get_next_vertex_extrapolated_to_wall(wall[0])
+
+        # First build a dummy polygon to track other surface
+        dummyPoly = Polygon(increment=False)
+        for vertex in wall[0].vertices(-1::-1):
+            dummyPoly.add_vertex(vertex)
+        wallidx_intersect_upper = dummyPoly.get_next_vertex_extrapolated_to_wall(wall[0])
+
+        # Add relevant part of upper solid wall
+        for vertex in wall[0].indices[wallidx_intersect_lower:wallidx_intersect_upper+1]:
+            polys[num_closed_surfaces+2].add_vertex(vertex)
+
+        # Close the polygon
+        polys[num_closed_surfaces+2].add_vertex(wall[num_closed_surfaces].vertices[0])
+
+        ############
+        # Finally, the zones between the solid wall and the open flux surfaces
+        for ipoly in range(num_closed_surfaces+2,num_closed_surfaces+2+num_open_surfaces)
+            polys.append( Polygon() )
+            opensurf_idx = ipoly-1
+            for vertex in wall[opensurf_idx].vertices:
+                polys[ipoly].add_vertex(vertex)
+            wallidx_intersect_bl = polys[ipoly].get_next_vertex_extrapolated_to_wall(wall[0])
+            
+            dummyPoly = Polygon(increment=False)
+            for vertex in wall[opensurf_idx-1].vertices:
+                dummyPoly.add_vertex(vertex)
+            wallidx_intersect_br = dummyPoly.get_next_vertex_extrapolated_to_wall(wall[0])
+
+            for vertex in wall[0].vertices[wallidx_intersect_bl:wallidx_intersect_br+1]:
+                polys[ipoly].add_vertex(vertex)
+
+            for vertex in wall[opensurf_idx-1].vertices[-1::-1]:
+                polys[ipoly].add_vertex(vertex)
+
+            wallidx_intersect_ur = polys[ipoly].get_next_vertex_extrapolated_to_wall(wall[0])
+
+            dummyPoly = Polygon(increment=False)
+            for vertex in wall[opensurf_idx].vertices[-1::-1]:
+                dummyPoly.add_vertex(vertex)
+            wallidx_intersect_ul = dummyPoly.get_next_vertex_extrapolated_to_wall(wall[0])
+
+            for vertex in wall[0].vertices[wallidx_intersect_ur:wallidx_intersect_ul-1:-1]:
+                polys[ipoly].add_vertex(vertex)
+
+            polys[ipoly].add_vertex(wall[opensurf_idx].vertices[0])
+
+    else:
+        print("Error: topology "+topology+" not defined.")
+        sys.exit(0)
+
--- wrapper/polygon.py
+++ wrapper/polygon.py
@@ -1,4 +1,150 @@
+import numpy as np
+import copy
 
-class polygon:
-    def __init__():
+# A polygon is an ordered set of vertices, connected by segments which close in on itself.
+class Polygon:
+    numPolygons = 0 # Polygons start counting from 1
 
+    def __init__(self,increment=True):
+        self.vertices = []
+        if increment:
+            Polygon.numPolygons += 1
+        self.id = Polygon.numPolygons
+
+    def addvertex(self,vertex):
+        self.vertices.append(vertex)
+
+    def clear_numPolygon():
+        Polygon.numPolygons = 0
+
+    # Linearly extrapolate the last two vertices defined for this polygon
+    # and find the point on the specified surface which is closest to said line.
+    # Returns index of said point
+    def get_next_vertex_extrapolated_to_wall(self,wall):
+        point1 = self.vertices[-1]
+        point2 = self.vertices[-2]
+
+        dir = [int( (point2[0]-point1[0])/abs(point2[0]-point1[0])),
+                int((point2[1]-point1[1])/abs(point2[1]-point1[1])) ]
+
+        first = True
+        idx = 0
+        for vertex in wall.vertices:
+            # Only check points that are on the correct half-plane according to 
+            # the directionality of the last two points
+            if ((dir[0]*vertex.coords[0]) > point1[0]) and ((dir[1]*vertex.coords[1]) > point1[1]):
+                current_distance_sq = ( (point2[1]-point1[1])*vertex.coords[0] 
+                    - (point2[0]-point1[0])*vertex.coords[1] 
+                    + point2[0]*point1[1] - point2[1]*point1[0])**2 \
+                    / ( (point2[1]-point1[1])**2 + (point2[0]-point1[0])**2 )
+                    
+                if first:
+                    closest_vertex=vertex
+                    closest_distance_sq = current_distance_sq
+                    first = False
+                elif current_distance_sq < cloest_distance_sq:
+                        closest_vertex = vertex
+                        closest_distance_sq = current_distance_sq
+            idx +=1
+
+        return idx-1
+
+    # Adds surfaces to construct a polygon. Entire is an array to determine which surfaces
+    # get added in their entirity. 
+    # If entire[i] = True, the entire surface[i] gets added (e.g. an open or closed flux surface)
+    # If entire[i] = False, only a subset that gets added. Which points to add are determined by 
+    #                extrapolating from the previous and next entire surface
+    def construct_from_surfaces(self,surfaces,entire):
+        if (len(surfaces) == 2) and surfaces[0].closed and surfaces[1].closed:
+            # To deal with two closed surfaces, connect the first points of each to form polygon
+        else:
+            reverse = False
+            for isurf in range(0,len(surfaces)):
+                if entire[isurf]:
+                    for vertex in surfaces[isurf].vertices:
+                        self.add_vertex(vertex)
+                    # Reverse direction for next entire surface
+                    reverse = not reverse 
+                else:
+                    if isurf == 0:
+                        print("Error. Need to start with an entire surface in call to construct_from_surfaces. Don't know how to begin defining polygon.")
+                        sys.exit(0)
+                    idx = self.add_next_vertex_extrapolated_to_wall(surfaces[isurf])
+
+                    
+
+
+
+                self.add
+                if surf.closed:
+
+        
+
+
+# A surface is also an ordered set of vertices, but can be open or closed
+# The vertices that make up various surfaces are combined to make a polygon
+class Surface:
+    numSurfaces = 0
+    def __init__(self,id_in):
+        self.vertices = []
+        Surface.numSurfaces +=1
+        self.id = Surface.numSurfaces
+        self.closed = True
+
+    def make_open(self):
+        self.closed = False
+    def make_closed(self):
+        self.closed = True
+
+    # Find the N vertices on surface that are closest to given vertex
+    def get_N_closest_vertices(self,N,vertex_in)
+        first = True
+        surf_use = copy.deepcopy(self)
+        closest_idxs = []
+        for i in range(0,N):
+            for iv in len(self.vertices):
+                distance_sq = (self.vertices[iv].coords[0]-vertex_in[0])**2 + \
+                        (self.vertices[iv].coords[1]-vertex_in[1])**2 
+                if first:
+                    first=False
+                    closest_distance_sq= distance_sq
+                    closest_vertex_idx = iv
+                    closest_vertex = self.vertices[iv]
+                elif distance_sq < closest_distance_sq:
+                    closest_distance_sq= distance_sq
+                    closest_vertex_idx = iv
+                    closest_vertex = self.vertices[iv]
+            closest_idxs.append(closest_vertex_idx)
+            surf_use.vertices = surf_use.vertices.remove[closest_vertex]
+        return closest_idxs
+
+
+    def addvertex(self,vertex):
+        self.vertices.append(vertex)
+
+    # Take the given index, and add it the surface at the appropriate point
+    # (as determined by the two closest vertices on the surface)
+    def insert_vertex_into_surface(self,vertex_in):
+        closest_idxs = self.get_N_closest_vertices(2,vertex_in)
+
+        if closest_idxs[1] > closest_idxs[0]:
+            idx_use = closest_idxs[1]
+        else:
+            idx_use = closest_idxs[0]
+            self.vertices.insert(closest_idxs[0],vertex_in)
+        self.vertices.insert(idx_use,vertex_in)
+
+        return idx_use
+
+    def clear_numSurface():
+        Surface.numSurfaces=0
+
+class Vertex:
+    def __init__(self,id_in,R,Z):
+        self.coords = [R,Z]
+        self.id = id_in 
+
+class Wall_vertex(Vertex,wallid,vertex,R,Z):
+    def __init__(self):
+        self.id = [wallid,vertex]
+        super().__init__(self.id,R,Z)
