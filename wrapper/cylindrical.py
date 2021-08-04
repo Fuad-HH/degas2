8f8cd7f833bc364abd4b8ef1b99fa999267843d5
George J. Wilkie <george.wilkie@gmail.com>
2021-08-04 10:58:41 -0400

    Put temperature of source in defineback and fixed source type to puff for cylindrical workflow.

--- wrapper/cylindrical.py
+++ wrapper/cylindrical.py
@@ -74,6 +74,6 @@ def process_output(NR,outputfilename="output.nc",tallyfilename="tally.nc",geomet
 def write_cylinder_input(R_tot,NR,source,ne,Te,material,TiTe_ratio=1.0,Nflights=10000,Ntheta_min=12,Ntheta_max=200,walltemp=300.0):
     rgrid = dg2d.write_cylinder_dg2d_input(R_tot,NR,material,Ntheta_min=Ntheta_min,Ntheta_max=Ntheta_max,walltemp=walltemp)
 
-    defineback.write_cylinder_db_input(rgrid,ne,Te,TiTe_ratio,source,R_tot,NR,Nflights)
+    defineback.write_cylinder_db_input(rgrid,ne,Te,TiTe_ratio,source,R_tot,NR,Nflights,walltemp=walltemp)
     return rgrid
 
