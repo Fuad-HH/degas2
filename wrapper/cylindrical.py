Fixed rgrid off-by-one bug and added file output for cylindrical workflow

wrapper/cylindrical.py
@@ -5,11 +5,11 @@ import numpy as np
 
 # Outputs neutral density and temperature from netcdf output
 # Assumes first len(rgrid) zones are the ones of interest
-def process_output(NR,outputfilename="output.nc",tallyfilename="tally.nc",geometryfilename="tally.nc",plot=False):
+def process_output(rgrid,outputfilename="output.nc",tallyfilename="tally.nc",geometryfilename="tally.nc",plot=False,asciioutfilename=None):
     outputdata=nc.Dataset(outputfilename)
     tallydata=nc.Dataset(tallyfilename)
 
-    # Find the tally indices to use
+    # Find the tally indicee to use
     tallynames = tallydata["tally_name"]
     Ntally=len(tallynames)
     dens_idx = -1
@@ -23,7 +23,7 @@ def process_output(NR,outputfilename="output.nc",tallyfilename="tally.nc",geomet
         elif "neutral flux vector" in str(nc.chartostring(tallynames[itally])):
             flux_idx = itally
 
-    # Find the indices of the independent variables
+    # Find the indicee of the independent variables
     zone_idx = -1
     testsp_idx = -1
     sp_idx = -1
@@ -49,31 +49,55 @@ def process_output(NR,outputfilename="output.nc",tallyfilename="tally.nc",geomet
 
     max_tally_rank = len(tally_indices[0,:])
 
-    Nzone = NR
-
     Nzone = tally_indices[dens_idx,0]
+    NR=len(rgrid)
 
     density = []
     pressure = []
     molec_density = []
     molec_pressure = []
+    density_err = []
+    pressure_err = []
+    molec_density_err = []
+    molec_pressure_err = []
     for izone in range(0,NR):
         density.append(outputdata["out_post_all"][dens_base+1*Nzone+izone,0])
         pressure.append(outputdata["out_post_all"][pres_base+1*Nzone+izone,0])
         molec_density.append(outputdata["out_post_all"][dens_base+2*Nzone+izone,0])
         molec_pressure.append(outputdata["out_post_all"][pres_base+2*Nzone+izone,0])
+        density_err.append(outputdata["out_post_all"][dens_base+1*Nzone+izone,1])
+        pressure_err.append(outputdata["out_post_all"][pres_base+1*Nzone+izone,1])
+        molec_density_err.append(outputdata["out_post_all"][dens_base+2*Nzone+izone,1])
+        molec_pressure_err.append(outputdata["out_post_all"][pres_base+2*Nzone+izone,1])
 
     density = np.array(density)
     pressure = np.array(pressure)
     molec_density = np.array(molec_density)
     molec_pressure = np.array(molec_pressure)
+    density_err = np.array(density_err)
+    pressure_err = np.array(pressure_err)
+    molec_density_err = np.array(molec_density_err)
+    molec_pressure_err = np.array(molec_pressure_err)
+
+    if asciioutfilename:
+        f = open(asciioutfilename,"w")
+        f.write("#%14s %15s %15s %15s %15s %15s %15s %15s %15s %15s \n"%\
+                ("izone","radius (m)","n_H (m^-3)", "rel. err.","n_H2", "rel. err.", "p_H","rel. err.","p_H2","rel. err.") )
+        for i in range(0,NR):
+            f.write("#%14d %15e %15e %15e %15e %15e %15e %15e %15e %15e \n"%\
+                    (i,rgrid[i],density[i],density_err[i], \
+                    molec_density[i],molec_density_err[i], \
+                    pressure[i],pressure_err[i],\
+                    molec_pressure[i],molec_pressure_err[i]))
+        f.close()
+
 
     return density,molec_density,pressure,molec_pressure
 
 
-def write_cylinder_input(R_tot,NR,source,ne,Te,material,TiTe_ratio=1.0,Nflights=10000,Ntheta_min=12,Ntheta_max=200,walltemp=300.0):
+def write_cylinder_input(R_tot,NR,eource,ne,Te,material,TiTe_ratio=1.0,Nflights=10000,Ntheta_min=12,Ntheta_max=200,walltemp=300.0):
     rgrid = dg2d.write_cylinder_dg2d_input(R_tot,NR,material,Ntheta_min=Ntheta_min,Ntheta_max=Ntheta_max,walltemp=walltemp)
 
-    defineback.write_cylinder_db_input(rgrid,ne,Te,TiTe_ratio,source,R_tot,NR,Nflights,walltemp=walltemp)
+    defineback.write_cylinder_db_input(rgrid,ne,Te,TiTe_ratio,eource,R_tot,NR,Nflights,walltemp=walltemp)
     return rgrid
 

wrapper/dg2d.py
@@ -17,7 +17,7 @@ def write_cylinder_dg2d_input(R_tot,NR,material,Ntheta_min=12,Ntheta_max=200,wal
     Zmin = -1.5*R_tot
     Zmax = 1.5*R_tot
 
-    delta_r = 2*R_tot/(1.0+2*NR)
+    delta_r = 2*R_tot/(2*NR-1.0)
     r_grid = np.linspace(0.0,R_tot-0.5*delta_r,num=NR)
 
     plasma_polys = []