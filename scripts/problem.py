050acefe5e1d3996c0663aedaf8d3bc01eb42495
George J. Wilkie <george.wilkie@gmail.com>
2023-05-19 16:32:32 -0400

    Modified default xgc reactions to include recombination.

--- scripts/problem.py
+++ scripts/problem.py
@@ -169,7 +169,7 @@ def genStdProblem(label):
     if label == "C-H":
         p=generateProblemInput(["0","H"],["e","H+"],["hionize5","hh_chargex"],["C"],["hdesorbc_xgc","hreflc"])
     if label == "C-D-xgc":
-        p=generateProblemInput(["0","D","D2","D2+"],["e","D+"],["hionize5","dchex_const","h2dis","h2ion","h2dision","h2pdision","h2pdis","h2pdisrec"],["C"],["hdesorbc","h2desorbc","dreflc"])
+        p=generateProblemInput(["0","D","D2","D2+"],["e","D+"],["hionize5","dchex_const","h2dis","h2ion","h2dision","h2pdision","h2pdis","h2pdisrec","hrecombine5"],["C"],["hdesorbc","h2desorbc","dreflc"])
     if label == "C":
         p=generateProblemInput(["0","H","H2","H2+"],["e","H+"],["hionize5","hh_chargex","h2dis","h2ion","h2dision","h2pdision","h2pdis","h2pdisrec"],["C"],["hdesorbc","h2desorbc","hreflc"])
     if label == "Li":
