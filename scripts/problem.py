dc7b575bdf41f7a156a83c51301a755034a20766
George J. Wilkie <george.wilkie@gmail.com>
2023-05-19 12:47:54 -0400

    Added new standard problem

--- scripts/problem.py
+++ scripts/problem.py
@@ -168,6 +168,8 @@ def genStdProblem(label):
     """
     if label == "C-H":
         p=generateProblemInput(["0","H"],["e","H+"],["hionize5","hh_chargex"],["C"],["hdesorbc_xgc","hreflc"])
+    if label == "C-D-xgc":
+        p=generateProblemInput(["0","D","D2","D2+"],["e","D+"],["hionize5","dchex_const","h2dis","h2ion","h2dision","h2pdision","h2pdis","h2pdisrec"],["C"],["hdesorbc","h2desorbc","dreflc"])
     if label == "C":
         p=generateProblemInput(["0","H","H2","H2+"],["e","H+"],["hionize5","hh_chargex","h2dis","h2ion","h2dision","h2pdision","h2pdis","h2pdisrec"],["C"],["hdesorbc","h2desorbc","hreflc"])
     if label == "Li":
